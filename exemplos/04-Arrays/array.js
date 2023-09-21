926b907bd9a1f4e9459835da7159892612acc67c
giuliavasconcelos <giuliavasconcelos.sesisenai@gmail.com>
2023-09-21 10:56:25 -0300

    array

--- exemplos/04-Arrays/array.js
+++ exemplos/04-Arrays/array.js
@@ -8,7 +8,13 @@ document.write('<h2 style="color: blue;">Criando um array (ou vetor)</h2><br>');
 // 1ª Digitação (Aqui)
 
 
-document.write("__________________________________________________________________<br>");
+document.write("Os melhores profissionais de F1 são: " +equipe+ "<br><br>");
+
+document.write("Os melhores chefe de F1 é: " +equipe[0]+ "<br>");
+document.write("A melhor estrategista de F1 é: <strong>" +equipe[1]+ "</strong><br>");
+document.write("O melhor engenheiro de F1 é: <strong>" +equipe[2]+ "</strong><br><br>");
+document.write("A equipe n°1 de F1 é: <strong>" +equipe[3]+ "</strong> sem sombra d dúvidas.<br>");
+
 
 // ______________________________________________________________________________________
 
@@ -28,3 +34,5 @@ let equipesF1 = [
 // Exibir Red Bull
 
 // 2ª Digitação (Aqui)
+
+document.write(`Melhor equipe de F1 da atualidade: "<strong>${equipesF1[0][2]}</strong><br>`);
--- exemplos/05-Objetos/objetos.js
+++ exemplos/05-Objetos/objetos.js
@@ -1,5 +1,13 @@
-// Exemplo 1: objeto básico
+// Exemplo 1: objeto básico`
 
+let atriz = {
+    nome: 'Angelina Jolie',
+    nascimento: 1975,
+    filmes: 60,
+    nacionalidade: 'Estados Unidos'
+};
+
+0
 
 // 1ª Digitação (Aqui)
 
@@ -25,6 +33,21 @@ document.write('<h2 style="color: blue;">Objeto contendo array e outro objeto</h
 
 // 2ª Digitação (Aqui)
 
+let celebridade = {
+    nome: 'Scarlett Johansson',
+    idade: 37,
+    sexo: 'feminino',
+    filmes: ['Lost in tralation', 'The Avengers', 'Marriage Story'],
+    cidade: 'Nova Iorque',
+    medidas: {
+        peso: 57,
+        altura: 1.60,
+    }
+};
+
+document.write(`<strong>${celebridade.nome}</strong> é uma atriz incrível, assista o filme: <strong>${celebridade.filmes[2]}</strong><br>`);
+
+
 
 document.write("__________________________________________________________________<br>");
 
@@ -35,6 +58,25 @@ document.write('<h2 style="color: blue;">Array do objeto</h2><br>');
 
 
 // 3ª Digitação (Aqui)
+
+let filmesFamosos = [
+    {
+        título: 'Pulp Fiction',
+        diretor: 'Quentin Tarantino',
+    },
+
+    {
+        título: 'The Godfather',
+        diretor: 'Francis Ford Coppola',
+    },
+
+    {
+        título: 'Inception',
+        diretor: 'Christopher Nolan',
+    }
+];
+
+document.write(`Meu filme favorito é: <strong>${filmeFamosos[1].título} </strong> dirigido por <strong>${filmesFamosos[1].diretor} </strong>`);
 
 
 
