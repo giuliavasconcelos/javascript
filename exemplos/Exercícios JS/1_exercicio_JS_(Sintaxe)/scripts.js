570d7a02548ffe43b4e848c7e093c3f8164b9b02
giuliavasconcelos <giuliavasconcelos.sesisenai@gmail.com>
2023-09-21 12:59:23 -0300

    js

--- exemplos/Exercícios JS/1_exercicio_JS_(Sintaxe)/scripts.js
+++ exemplos/Exercícios JS/1_exercicio_JS_(Sintaxe)/scripts.js
@@ -2,3 +2,34 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
 // Digite a resolução do exercício aqui e visualize o resultado no navegador
+
+document.write('<h2 style="color: red;">Resolução</h2><br>');
+
+
+// Digite a resolução do exercício aqui e visualize o resultado no navegador
+
+// 1º Exemplo
+document.write("Quase almoço!<br>");
+
+// 2º Exemplo
+    let nome = "Giulia Santos Vasconcelos";
+
+document.write(`Meu nome é <strong>${nome}</strong>.<br>`)
+
+// 3º Exemplo
+
+let produto = 'Celular Iphone da Apple';
+let precoFret = 54;
+let precoCel = 8200;
+let parcelas = 12;
+let valorParcelas = 684;
+let total = precoCel + precoFret;
+
+
+document.write(`O melhor celular do mundo é: <strong>${produto}</strong> <br>`)
+document.write(`Custando: <strong>${precoCel}</strong> <br>`)
+document.write(`Frete custando: <strong>${precoFret}</strong> <br>`)
+document.write(`Podemos parcelar até em <strong>${parcelas}</strong> vezes.<br>`)
+document.write(`Custando um total de: <strong>${total}</strong> <br>`)
+
+
