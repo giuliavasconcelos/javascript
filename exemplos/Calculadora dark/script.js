bdab4bb737fdcb1a92d862b976fcd5e81d7d3a29
giuliavasconcelos <giuliavasconcelos.sesisenai@gmail.com>
2023-11-16 11:18:40 -0300

    calculor

--- exemplos/Calculadora dark/script.js
+++ exemplos/Calculadora dark/script.js
@@ -1,1 +1,33 @@
-/* Digite o JS aqui */
+// Seleciona o elemento HTML com o id 'inputbox' e armazena em 'input'
+let input = document.getElementById('inputBox');
+
+//Seleciona todos os elemntos HTML do tipo 'button' e armazena em 'buttons'
+let buttons = document.querySelectorAll('button');
+
+//Incia uma string vazia para armazenar a entrada do usuario
+let string = "";
+
+// Converte a coleção de botões em um array para conseguir percorrer sobre eles
+let arr = Array.from(buttons);
+
+// 
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        } 
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
